32d04dd5d64f8b7c7feb4c24939456ba5decd465
Lena05k <lena05k@yandex.ru>
2022-03-21 17:57:27 +0300

    ...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-### Hexlet tests and linter status:
+### frontend-project-lvl2
+
 [![Actions Status](https://github.com/Lena05k/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Lena05k/frontend-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import { genDiff } from '../src/Diff.js';
 const program = new Command();
 program
 
@@ -9,8 +10,7 @@ program
     .argument('<filepath2>')
     .option('-f, --format [type]', 'output format')
     .action((filepath1, filepath2) => {
-        console.log(filepath1);
-        console.log(filepath2);
+        console.log(genDiff(filepath1, filepath2));
     })
     .description('Compares two configuration files and shows a difference.');
 
