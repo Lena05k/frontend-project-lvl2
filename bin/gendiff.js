c3314d7cc5d59b1013b2528b836de8b165bcc3c0
Lena05k <lena05k@yandex.ru>
2022-04-04 13:28:37 +0300

    Step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
-import { genDiff } from '../src/Diff.js';
+import { Command } from 'commander';
+import genDiff  from '../src/index.js';
 const program = new Command();
-program
 
+program
     .version('0.0.1', '-V, --version', 'output the version number')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
+    .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
     .action((filepath1, filepath2) => {
         console.log(genDiff(filepath1, filepath2));
-    })
-    .description('Compares two configuration files and shows a difference.');
-
-program.parse();
+    });
+program.parse(process.argv);
