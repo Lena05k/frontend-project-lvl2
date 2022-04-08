28920516cf32d150e8a2b3f25ff1f5c11500af0c
Lena05k <lena05k@yandex.ru>
2022-04-08 14:06:25 +0300

    Step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import genDiff  from '../src/index.js';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1>')
-    .arguments('<filepath2>')
-    .action((filepath1, filepath2) => {
-        console.log(genDiff(filepath1, filepath2));
-    });
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import { resolve } from 'path';
 import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
-    const file1 = JSON.parse(readFileSync(resolve(filepath1), 'utf-8'));
-    const file2 = JSON.parse(readFileSync(resolve(filepath2), 'utf-8'));
+  const file1 = JSON.parse(readFileSync(resolve(filepath1), 'utf-8'));
+  const file2 = JSON.parse(readFileSync(resolve(filepath2), 'utf-8'));
 
-    const keys = Object.keys({...file1, ...file2}).sort();
+  const keys = Object.keys({ ...file1, ...file2 }).sort();
 
-    const getDistinction = keys.map((key) => {
-         if (!_.has(file2, key)) {
-          return `- ${key}: ${file1[key]}`;
-      }
-      if (!_.has(file1, key)) {
-          return `+ ${key}: ${file2[key]}`;
-      }
-      if (file1[key] === file2[key]) {
-          return `  ${key}: ${file1[key]}`;
-      }
-      return `- ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`
-    }).join('\n  ');
-     console.log(`{\n  ${getDistinction}\n}`);
-}
+  const getDistinction = keys.map((key) => {
+    if (!_.has(file2, key)) {
+      return `- ${key}: ${file1[key]}`;
+    }
+    if (!_.has(file1, key)) {
+      return `+ ${key}: ${file2[key]}`;
+    }
+    if (file1[key] === file2[key]) {
+      return `  ${key}: ${file1[key]}`;
+    }
+    return `- ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
+  }).join('\n  ');
+  console.log(`{\n  ${getDistinction}\n}`);
+};
 
 export default genDiff;
