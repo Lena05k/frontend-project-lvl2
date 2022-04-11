88ec4868137a82216d29e01d31a209ccb55558f5
Lena05k <lena05k@yandex.ru>
2022-04-11 12:50:42 +0300

    Add jest-test

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+import genDiff from '../src/index.js';
+import {getFixturePath} from "../src/test-file-path.js";
+
+const filepath1 = getFixturePath('file1.json');
+const filepath2 = getFixturePath('file2.json');
+const expected = `{
+      - follow: false
+        host: hexlet.io
+      - proxy: 123.234.53.22
+      - timeout: 50
+      + timeout: 20
+      + verbose: true
+    }`;
+
+test('genDiff', () => {
+    expect(genDiff(filepath1, filepath2)).toEqual(expected);
+});
---
+++ src/test-file-path.js
@@ -0,0 +1,7 @@
+import {fileURLToPath} from "url";
+import path, {dirname} from "path";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
