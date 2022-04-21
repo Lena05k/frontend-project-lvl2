8094c46e23f93df30388a62e19042df7994d2b40
Lena05k <lena05k@yandex.ru>
2022-04-21 20:41:45 +0300

    Comparison of two files.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { resolve, extname } from 'path';
 import _ from 'lodash';
+import parseFile from "./parse.js";
 
-const genDiff = (filepath1, filepath2) => {
-  const file1 = JSON.parse(readFileSync(resolve(filepath1), 'utf-8'));
-  const file2 = JSON.parse(readFileSync(resolve(filepath2), 'utf-8'));
+const genDiff = (filePath1, filePath2) => {
+  const extName1 = extname(filePath1);
+  const extName2 = extname(filePath2);
+  const readFile = (filePath) => readFileSync(resolve(filePath), 'utf-8');
+  const file1 = parseFile(readFile(filePath1), extName1);
+  const file2 = parseFile(readFile(filePath2), extName2);
 
   const keys = Object.keys({ ...file1, ...file2 }).sort();
 
@@ -20,6 +24,7 @@ const genDiff = (filepath1, filepath2) => {
     }
     return `- ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
   }).join('\n  ');
+
   return `{\n  ${getDistinction}\n}`;
 };
 
