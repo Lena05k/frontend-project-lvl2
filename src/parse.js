e6d434842a7b41b675f74dc4a0f5575dea770844
Lena05k <lena05k@yandex.ru>
2022-04-21 20:58:00 +0300

    Analyzes the file resolution.

--- src/parse.js
+++ src/parse.js
@@ -1,15 +1,15 @@
 import yaml from 'js-yaml';
 
 const parseFile = (data, format) => {
-    let parse;
-    if (format === '.json') {
-       parse = JSON.parse;
-    } else if (format === '.yml' || format === '.yaml') {
-        parse = yaml.load;
-    } else {
-        throw new Error(`The file extension "${format}" is not supported.`)
-    }
-    return parse(data);
-}
+  let parse;
+  if (format === '.json') {
+     parse = JSON.parse;
+  } else if (format === '.yml' || format === '.yaml') {
+      parse = yaml.load;
+  } else {
+      throw new Error(`The file extension "${format}" is not supported.`);
+  }
+  return parse(data);
+};
 
 export default parseFile;
