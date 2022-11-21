9f668ce3fccf73c89bd3868ba95fecb42369d4ae
Lena05k <lena05k@yandex.ru>
2022-11-21 18:51:27 +0300

    Fixed a syntax error.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const getData = (filePath) => readFileSync(path.resolve(`${process.cwd()}`, file
 const getExtension = (filePath) => path.extname(filePath).slice(1);
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-    const data1 = parse(getData(filepath1), getExtension(filepath1));
-    const data2 = parse(getData(filepath2), getExtension(filepath2));
-    return format(buildDiff(data1, data2), formatName)
-}
+  const data1 = parse(getData(filepath1), getExtension(filepath1));
+  const data2 = parse(getData(filepath2), getExtension(filepath2));
+  return format(buildDiff(data1, data2), formatName);
+};
 
 export default genDiff;
