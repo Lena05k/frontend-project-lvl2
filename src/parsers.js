26c008a68788ba742d2423f41dcb3b959c85a3c9
Lena05k <lena05k@yandex.ru>
2022-11-21 18:52:59 +0300

    Analyzes the file format.

---
+++ src/parsers.js
@@ -0,0 +1,15 @@
+import yaml from 'js-yaml';
+
+const parse = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unknown type of format: ${format}`);
+  }
+};
+
+export default parse;
