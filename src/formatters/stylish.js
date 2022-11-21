c12c3166e5ad942aafd40592c7978b337eeaf129
Lena05k <lena05k@yandex.ru>
2022-11-21 20:28:45 +0300

    Input style

---
+++ src/formatters/stylish.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+
+const stepIndent = 4;
+const getIndex = (count) => ' '.repeat(count * stepIndent);
+
+const getValue = (node, depth) => {
+  if (!_.isObject(node)) {
+    return node;
+  }
+  const bracketEndIndent = getIndex(depth - 1);
+  const lines = Object.entries(node).map(([key, value]) => `${getIndent(depth)}${key}: ${getValue(value, depth + 1)}`);
+
+  return ['{', ...lines, `${bracketEndIndent}}`].join('\n');
+};
+
+const stylish = (data, depth = 1) => {
+  const indent = getIndex(depth).slice(0, getIndex(depth) - 2);
+  const bracketEndIndent = getIndent(depth - 1);
+
+  const lines = data.flatMap((diff) => {
+    switch (diff.type) {
+      case 'nested':
+        return `${indent}  ${diff.key}: ${stylish(diff.children, depth + 1)}`;
+      case 'added':
+        return `${indent}+ ${diff.key}: ${getValue(diff.value2, depth + 1)}`;
+      case 'deleted':
+        return `${indent}- ${diff.key}: ${getValue(diff.value1, depth + 1)}`;
+      case 'unchanged':
+        return `${indent}  ${diff.key}: ${getValue(diff.value1, depth + 1)}`;
+      case 'changed':
+        return [
+          `${indent}- ${diff.key}: ${getValue(diff.value1, depth + 1)}`,
+          `${indent}+ ${diff.key}: ${getValue(diff.value2, depth + 1)}`,
+        ];
+      default:
+        throw new Error(`Unknown type of data: ${diff.type}`);
+    }
+  });
+  return [
+    '{', ...lines, `${bracketEndIndent}}`].join('\n');
+};
+
+export default stylish;
