8812c83eb9cd32311e0ad03260ba57185b68fb3d
Lena05k <lena05k@yandex.ru>
2022-11-21 18:49:50 +0300

    Added make gendiff

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 install:
 	npm ci
+
+gendiff:
+	node bin/gendiff.js
---
+++ bin/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
+
+program.parse();
