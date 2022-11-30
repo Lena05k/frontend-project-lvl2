c5b09dc8851ea38f745271efe6d97378906ad190
Lena05k <lena05k@yandex.ru>
2022-11-30 21:19:32 +0300

    Changed getExtension to getFormat.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import buildDiff from './buildDiff.js';
 import format from './formatters/index.js';
 
 const getData = (filePath) => readFileSync(path.resolve(`${process.cwd()}`, filePath));
-const getExtension = (filePath) => path.extname(filePath).slice(1);
+const getFormat = (filePath) => path.extname(filePath).slice(1);
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parse(getData(filepath1), getExtension(filepath1));
-  const data2 = parse(getData(filepath2), getExtension(filepath2));
+  const data1 = parse(getData(filepath1), getFormat(filepath1));
+  const data2 = parse(getData(filepath2), getFormat(filepath2));
   return format(buildDiff(data1, data2), formatName);
 };
 
