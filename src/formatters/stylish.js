0c7f56739801439049bce4f837909041b81c2c89
Lena05k <lena05k@yandex.ru>
2022-12-01 13:14:40 +0300

    Improved the stylish function.

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,51 +1,36 @@
 import _ from 'lodash';
 
-const stepIndent = 4;
-const getIndent = (count) => ' '.repeat(count * stepIndent);
-
-const getValue = (node, depth) => {
+const genIndent = (depth, str = ' ', spacesCount = 4) => str.repeat((depth * spacesCount) - 2);
+const stringify = (node, depth = 1) => {
   if (!_.isObject(node)) {
     return node;
   }
-  const bracketEndIndent = getIndent(depth - 1);
-  const lines = Object.entries(node).map(([key, value]) => `${getIndent(depth)}${key}: ${getValue(value, depth + 1)}`);
+  const lines = Object.entries(node).map(([key, value]) => `${genIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
 
-  return [
-    '{',
-    ...lines,
-    `${bracketEndIndent}}`,
-  ].join('\n');
+  return `{\n${lines.join('\n')}\n  ${genIndent(depth)}}`;
 };
 
-const stylish = (data, depth = 1) => {
-  const indent = getIndent(depth).slice(0, getIndent(depth) - 2);
-  const bracketEndIndent = getIndent(depth - 1);
-
-  const lines = data.flatMap((diff) => {
-    switch (diff.type) {
-      case 'nested':
-        return `${indent}  ${diff.key}: ${stylish(diff.children, depth + 1)}`;
-      case 'added':
-        return `${indent}+ ${diff.key}: ${getValue(diff.value2, depth + 1)}`;
-      case 'deleted':
-        return `${indent}- ${diff.key}: ${getValue(diff.value1, depth + 1)}`;
-      case 'unchanged':
-        return `${indent}  ${diff.key}: ${getValue(diff.value1, depth + 1)}`;
-      case 'changed':
-        return [
-          `${indent}- ${diff.key}: ${getValue(diff.value1, depth + 1)}`,
-          `${indent}+ ${diff.key}: ${getValue(diff.value2, depth + 1)}`,
-        ];
-      default:
-        throw new Error(`Unknown type of data: ${diff.type}`);
-    }
-  });
-
-  return [
-    '{',
-    ...lines,
-    `${bracketEndIndent}}`,
-  ].join('\n');
+const stylish = (data) => {
+  const iter = (node, depth = 1) => {
+    const lines = node.map((diff) => {
+      switch (diff.type) {
+        case 'nested':
+          return `${genIndent(depth)}  ${diff.key}: {\n${iter(diff.children, depth + 1)}\n${genIndent(depth)}  }`;
+        case 'added':
+          return `${genIndent(depth)}+ ${diff.key}: ${stringify(diff.value2, depth)}`;
+        case 'deleted':
+          return `${genIndent(depth)}- ${diff.key}: ${stringify(diff.value1, depth)}`;
+        case 'unchanged':
+          return `${genIndent(depth)}  ${diff.key}: ${stringify(diff.value1, depth)}`;
+        case 'changed':
+          return `${genIndent(depth)}- ${diff.key}: ${stringify(diff.value1, depth)}\n${genIndent(depth)}+ ${diff.key}: ${stringify(diff.value2, depth)}`;
+        default:
+          throw new Error(`Unknown type of data: ${diff.type}`);
+      }
+    });
+    return lines.join('\n');
+  };
+  return `{\n${iter(data)}\n}`;
 };
 
 export default stylish;
